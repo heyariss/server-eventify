98339100d583966f4f49d6473f1af0e65052779b
riski aris sandy <riskiariss561@gmail.com>
2024-01-09 19:30:00 +0700

    [FEAT] images and avatars

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const app = express();
 
 // Router
 const categoriesRouter = require('./app/api/v1/categories/router');
+const imagesRouter = require('./app/api/v1/images/router');
+
 const v1 = '/api/v1/cms';
+
+// middlewares
+const notFoundMiddleware = require('./app/middlewares/not-found');
+const handleErrorMiddleware = require('./app/middlewares/handler-error');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,5 +28,10 @@ app.get('/', (req, res) => {
 });
 
 app.use(v1, categoriesRouter);
+app.use(v1, imagesRouter);
+
+// middlewares
+app.use(notFoundMiddleware);
+app.use(handleErrorMiddleware);
 
 module.exports = app;
